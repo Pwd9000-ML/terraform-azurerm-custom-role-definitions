d012e2d3d8fd88c6e69992b577a02b2bde916f8d
Pwd9000 <pwd9000@hotmail.co.uk>
2022-11-16 11:07:37 +0000

    update test

--- tests/auto_test1/main.tf
+++ tests/auto_test1/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 # MODULES                                        #
 ##################################################
 module "custom_roles" {
-  source                  = "github.com/Pwd9000-ML/terraform-azurerm-custom-role-definitions"
+  source                  = "../.."
   count                   = var.deploy_custom_roles == true ? 1 : 0
   custom_role_definitions = var.custom_role_definitions
 }
