5d3fb93f99e9ea79fae3d20d9f71158c20733230
Pwd9000-ML <pwd9000@hotmail.co.uk>
2023-07-14 22:51:28 +0100

    up

--- examples/example_01/variables.tf
+++ examples/example_01/variables.tf
@@ -9,6 +9,31 @@ variable "deploy_custom_roles" {
 }
 
 variable "custom_role_definitions" {
-  type        = list(any)
-  description = "Specifies a list of custom roles"
+  type = list(object({
+    role_definition_name = string
+    scope                = string
+    description          = string
+    permissions = object({
+      actions          = list(string)
+      data_actions     = list(string)
+      not_actions      = list(string)
+      not_data_actions = list(string)
+    })
+    assignable_scopes = list(string)
+  }))
+  default = [
+    {
+      role_definition_name = "CUSTOM Role Name"
+      description          = "description of the role"
+      scope                = ""
+      permissions = {
+        actions          = ["Microsoft.Web/sites/config/list/action", "Microsoft.Web/sites/config/read"]
+        data_actions     = []
+        not_actions      = []
+        not_data_actions = []
+      }
+      assignable_scopes = []
+    }
+  ]
+  description = "Specifies a list of custom role definitions"
 }
--- tests/auto_test1/testing.auto.tfvars
+++ tests/auto_test1/testing.auto.tfvars
@@ -4,16 +4,19 @@ custom_role_definitions = [
   {
     role_definition_name = "CUSTOM - App Settings Reader"
     description          = "Allows view access for Azure Sites Configuration"
+    scope                = ""
     permissions = {
       actions          = ["Microsoft.Web/sites/config/list/action", "Microsoft.Web/sites/config/read"]
       data_actions     = []
       not_actions      = []
       not_data_actions = []
     }
+    assignable_scopes = []
   },
   {
     role_definition_name = "CUSTOM - App Settings Admin"
     description          = "Allows edit access for Azure Sites Configuration"
+    scope                = ""
 
     permissions = {
       actions          = ["Microsoft.Web/sites/config/*"]
@@ -21,5 +24,6 @@ custom_role_definitions = [
       not_actions      = []
       not_data_actions = []
     }
+    assignable_scopes = []
   }
 ]
