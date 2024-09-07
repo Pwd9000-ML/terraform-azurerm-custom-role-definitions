68ad0dd5cec1a272ec48a728e99db19a5f6e8d72
Pwd9000-ML <pwd9000@hotmail.co.uk>
2024-09-07 13:14:34 +0100

    chore: Update hashicorp/azurerm requirement to latest version
    This commit updates the hashicorp/azurerm requirement from ~> 3.113.0 to ~> 4.0.1. The update allows for the use of the latest version of the hashicorp/azurerm provider.
    
    [Release notes](https://github.com/hashicorp/terraform-provider-azurerm/releases)
    [Changelog](https://github.com/hashicorp/terraform-provider-azurerm/blob/main/CHANGELOG.md)
    [Commits](https://github.com/hashicorp/terraform-provider-azurerm/compare/v3.113.0...v4.0.1)

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@
 # OUTPUTS                                        #
 ##################################################
 output "role_definition_ids" {
-  value       = azurerm_role_definition.custom_role.*.role_definition_id
+  value       = azurerm_role_definition.custom_role[*].role_definition_id
   description = "List of Role Definition IDs."
 }
 
 output "role_definition_resource_ids" {
-  value       = azurerm_role_definition.custom_role.*.role_definition_resource_id
+  value       = azurerm_role_definition.custom_role[*].role_definition_resource_id
   description = "List of Azure Resource Manager IDs for the resources."
 }
